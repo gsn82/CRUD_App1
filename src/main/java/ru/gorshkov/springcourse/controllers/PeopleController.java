b24e3bb2d2112633c05a065606d3cead7a094f9b
Gorshkov Sergey <Gsn82@mail.ru>
2021-05-15 17:35:32 +0500

    23

--- src/main/java/ru/gorshkov/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/gorshkov/springcourse/controllers/PeopleController.java
@@ -19,29 +19,50 @@ public class PeopleController {
         this.personDAO = personDAO;
     }
 
+    // получим всех людей из DAO и передадим на отображение в представление
     @GetMapping()
     public String index(Model model) {
-        // получим всех людей из DAO и передадим на отображение в представление
         model.addAttribute("people", personDAO.index());
         return "people/index";
     }
 
+    // получим одного человека по id из DAO и передадим на отображаение в представление
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model) {
-        // получим одного человека по id из DAO и передадим на отображаение в представление
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
     }
 
+
     @GetMapping("/new")
-    public String newPerson(Model model){
-        model.addAttribute("person", new Person());
-            return "people/new";
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
     }
 
     @PostMapping()
-    public String create(@ModelAttribute("person") Person person){
+    public String create(@ModelAttribute("person") Person person) {
         personDAO.save(person);
         return "redirect:/people";
     }
+
+    // получаем форму для ректирования people
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id){
+        model.addAttribute("person",personDAO.show(id));
+        return  "people/edit";
+    }
+
+    // отправляем данные для обновления people
+    @PatchMapping("/{id}")
+    public String update (@ModelAttribute("person") Person person, @PathVariable("id") int id){
+        personDAO.update(id, person);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id){
+        personDAO.delete(id);
+        return "redirect:/people";
+    }
+
 }
--- src/main/java/ru/gorshkov/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/gorshkov/springcourse/dao/PersonDAO.java
@@ -15,22 +15,34 @@ public class PersonDAO {
     {
         people = new ArrayList<>();
 
-        people.add(new Person(++PEOPLE_COUNT,"Tom"));
-        people.add(new Person(++PEOPLE_COUNT,"Bob"));
-        people.add(new Person(++PEOPLE_COUNT,"Mike"));
-        people.add(new Person(++PEOPLE_COUNT,"Katy"));
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy"));
     }
 
-    public List<Person> index(){
+    public List<Person> index() {
         return people;
     }
 
-    public Person show(int id){
-        return people.stream().filter(person -> person.getId()==id).findAny().orElse(null);
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
     }
 
-    public void save(Person person){
+    public void save(Person person) {
         person.setId(++PEOPLE_COUNT);
         people.add(person);
+    }
+
+    // обновление человека
+    public void update(int id, Person updatePerson) {
+        // нашли человека по id и его будем обнвлять
+        Person personToBeUpdate = show(id);
+        // т.к у человека только имя , только его и будем обновлять
+        personToBeUpdate.setName(updatePerson.getName());
+    }
+
+    public void delete(int id) {
+        people.removeIf(p -> p.getId() == id);
     }
 }
