a178df29bc554aae54c597ad6b251dcbe32ff684
Gorshkov Sergey <Gsn82@mail.ru>
2021-05-15 20:50:59 +0500

    24

--- src/main/java/ru/gorshkov/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/gorshkov/springcourse/controllers/PeopleController.java
@@ -4,9 +4,12 @@ package ru.gorshkov.springcourse.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.gorshkov.springcourse.dao.PersonDAO;
 import ru.gorshkov.springcourse.models.Person;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/people")
@@ -19,20 +22,17 @@ public class PeopleController {
         this.personDAO = personDAO;
     }
 
-    // получим всех людей из DAO и передадим на отображение в представление
     @GetMapping()
     public String index(Model model) {
         model.addAttribute("people", personDAO.index());
         return "people/index";
     }
 
-    // получим одного человека по id из DAO и передадим на отображаение в представление
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model) {
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
     }
-
 
     @GetMapping("/new")
     public String newPerson(@ModelAttribute("person") Person person) {
@@ -40,29 +40,36 @@ public class PeopleController {
     }
 
     @PostMapping()
-    public String create(@ModelAttribute("person") Person person) {
+    public String create(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "people/new";
+
         personDAO.save(person);
         return "redirect:/people";
     }
 
-    // получаем форму для ректирования people
     @GetMapping("/{id}/edit")
-    public String edit(Model model, @PathVariable("id") int id){
-        model.addAttribute("person",personDAO.show(id));
-        return  "people/edit";
+    public String edit(Model model,
+                       @PathVariable("id") int id) {
+        model.addAttribute("person", personDAO.show(id));
+        return "people/edit";
     }
 
-    // отправляем данные для обновления people
     @PatchMapping("/{id}")
-    public String update (@ModelAttribute("person") Person person, @PathVariable("id") int id){
+    public String update(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+        if (bindingResult.hasErrors())
+            return "people/edit";
+
         personDAO.update(id, person);
         return "redirect:/people";
     }
 
     @DeleteMapping("/{id}")
-    public String delete(@PathVariable("id") int id){
+    public String delete(@PathVariable("id") int id) {
         personDAO.delete(id);
         return "redirect:/people";
     }
-
 }
--- src/main/java/ru/gorshkov/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/gorshkov/springcourse/dao/PersonDAO.java
@@ -9,16 +9,15 @@ import java.util.List;
 @Component
 public class PersonDAO {
     private static int PEOPLE_COUNT;
-
     private List<Person> people;
 
     {
         people = new ArrayList<>();
 
-        people.add(new Person(++PEOPLE_COUNT, "Tom"));
-        people.add(new Person(++PEOPLE_COUNT, "Bob"));
-        people.add(new Person(++PEOPLE_COUNT, "Mike"));
-        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+        people.add(new Person(++PEOPLE_COUNT, "Tom", 24, "tom@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob", 52, "bob@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike", 18, "mike@yahoo.com"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy", 34, "katy@gmail.com"));
     }
 
     public List<Person> index() {
@@ -34,12 +33,12 @@ public class PersonDAO {
         people.add(person);
     }
 
-    // обновление человека
-    public void update(int id, Person updatePerson) {
-        // нашли человека по id и его будем обнвлять
-        Person personToBeUpdate = show(id);
-        // т.к у человека только имя , только его и будем обновлять
-        personToBeUpdate.setName(updatePerson.getName());
+    public void update(int id, Person updatedPerson) {
+        Person personToBeUpdated = show(id);
+
+        personToBeUpdated.setName(updatedPerson.getName());
+        personToBeUpdated.setAge(updatedPerson.getAge());
+        personToBeUpdated.setEmail(updatedPerson.getEmail());
     }
 
     public void delete(int id) {
